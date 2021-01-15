6c33ac80a3f2657da8cd21c3b0ef0d78b8d532aa
Melkia <bonfil99melkia@gmail.com>
2021-01-15 01:07:06 -0600

    put finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+// Routing
+import { AppRoutingModule } from 'src/app/app.routing.module';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+// import { HerosService } from "src/app/services/heros.service";
+
 // Importar los componentes
 import { AppComponent } from './app.component';
-import { HeaderComponent } from "./components/header/header.component";
-import { BodyComponent } from "./components/body/body.component";
-import { FooterComponent } from './components/footer/footer.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent
+    HeroeComponent,
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
